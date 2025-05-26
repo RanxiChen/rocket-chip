7759acb8eb484eb703bccdbf39ebaf2f632efe69
chenxiuzhuang <chenxiuzhuang_2002@163.com>
2025-05-26 12:27:57 +0800

    forked from python-data-rocket

--- src/main/scala/system/Configs.scala
+++ src/main/scala/system/Configs.scala
@@ -92,3 +92,422 @@ class BaseFPGAConfig extends Config(new BaseConfig ++ new WithCoherentBusTopolog
 class DefaultFPGAConfig extends Config(new WithNSmallCores(1) ++ new BaseFPGAConfig)
 
 class CloneTileConfig extends Config(new WithCloneRocketTiles(7) ++ new WithNBigCores(1) ++ new WithCoherentBusTopology ++ new BaseConfig)
+
+class BaseLitexConfig extends Config(
+  new WithLitexMemPort() ++
+  new WithLitexMMIOPort() ++
+  new WithLitexSlavePort ++
+  new WithNExtTopInterrupts(8) ++
+  new WithCoherentBusTopology ++
+  new BaseConfig
+)
+
+class WithLitexHextConfig extends Config(
+  new WithHypervisor ++
+  new WithBitManip ++ new WithBitManipCrypto ++
+  new WithCryptoNIST ++ new WithCryptoSM
+)
+
+class LitexConfig_small_1_1 extends Config(
+  new WithNSmallCores(1) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_1_2 extends Config(
+  new WithNSmallCores(1) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_1_4 extends Config(
+  new WithNSmallCores(1) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_1_8 extends Config(
+  new WithNSmallCores(1) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_2_1 extends Config(
+  new WithNSmallCores(2) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_2_2 extends Config(
+  new WithNSmallCores(2) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_2_4 extends Config(
+  new WithNSmallCores(2) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_2_8 extends Config(
+  new WithNSmallCores(2) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_4_1 extends Config(
+  new WithNSmallCores(4) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_4_2 extends Config(
+  new WithNSmallCores(4) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_4_4 extends Config(
+  new WithNSmallCores(4) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_4_8 extends Config(
+  new WithNSmallCores(4) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_8_1 extends Config(
+  new WithNSmallCores(8) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_8_2 extends Config(
+  new WithNSmallCores(8) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_8_4 extends Config(
+  new WithNSmallCores(8) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_small_8_8 extends Config(
+  new WithNSmallCores(8) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_1_1 extends Config(
+  new WithNMedCores(1) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_1_2 extends Config(
+  new WithNMedCores(1) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_1_4 extends Config(
+  new WithNMedCores(1) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_1_8 extends Config(
+  new WithNMedCores(1) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_2_1 extends Config(
+  new WithNMedCores(2) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_2_2 extends Config(
+  new WithNMedCores(2) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_2_4 extends Config(
+  new WithNMedCores(2) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_2_8 extends Config(
+  new WithNMedCores(2) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_4_1 extends Config(
+  new WithNMedCores(4) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_4_2 extends Config(
+  new WithNMedCores(4) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_4_4 extends Config(
+  new WithNMedCores(4) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_4_8 extends Config(
+  new WithNMedCores(4) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_8_1 extends Config(
+  new WithNMedCores(8) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_8_2 extends Config(
+  new WithNMedCores(8) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_8_4 extends Config(
+  new WithNMedCores(8) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_medium_8_8 extends Config(
+  new WithNMedCores(8) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_1_1 extends Config(
+  new WithNBigCores(1) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_1_2 extends Config(
+  new WithNBigCores(1) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_1_4 extends Config(
+  new WithNBigCores(1) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_1_8 extends Config(
+  new WithNBigCores(1) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_2_1 extends Config(
+  new WithNBigCores(2) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_2_2 extends Config(
+  new WithNBigCores(2) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_2_4 extends Config(
+  new WithNBigCores(2) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_2_8 extends Config(
+  new WithNBigCores(2) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_4_1 extends Config(
+  new WithNBigCores(4) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_4_2 extends Config(
+  new WithNBigCores(4) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_4_4 extends Config(
+  new WithNBigCores(4) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_4_8 extends Config(
+  new WithNBigCores(4) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_8_1 extends Config(
+  new WithNBigCores(8) ++
+  new WithMemoryDataBits(64) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_8_2 extends Config(
+  new WithNBigCores(8) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_8_4 extends Config(
+  new WithNBigCores(8) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_linux_8_8 extends Config(
+  new WithNBigCores(8) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_1_1 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(1) ++
+  new WithMemoryDataBits(64) ++
+  new WithInclusiveCache() ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_1_2 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(1) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_1_4 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(1) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_1_8 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(1) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_2_1 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(2) ++
+  new WithMemoryDataBits(64) ++
+  new WithInclusiveCache() ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_2_2 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(2) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_2_4 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(2) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_2_8 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(2) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_4_1 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(4) ++
+  new WithMemoryDataBits(64) ++
+  new WithInclusiveCache() ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_4_2 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(4) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_4_4 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(4) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_4_8 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(4) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_8_1 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(8) ++
+  new WithMemoryDataBits(64) ++
+  new WithInclusiveCache() ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_8_2 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(8) ++
+  new WithMemoryDataBits(128) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_8_4 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(8) ++
+  new WithMemoryDataBits(256) ++
+  new BaseLitexConfig
+)
+
+class LitexConfig_full_8_8 extends Config(
+  new WithLitexHextConfig ++
+  new WithNBigCores(8) ++
+  new WithMemoryDataBits(512) ++
+  new BaseLitexConfig
+)
